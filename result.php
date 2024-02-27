d0f1359693e463ac4c6d9c8c574cba1805944a1e
azizafarooq <azizafarooq2000@gmail.com>
2024-02-27 10:42:33 +0100

    feat: cards

--- result.php
+++ result.php
@@ -2,6 +2,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,39 +11,65 @@
     <title>Hotel Finder</title>
 </head>
 <style>
-    ul{
-        list-style: none;
-        padding-left: 0;
-        margin-top: 2rem
-    }
+
 </style>
+
 <body>
     <div class="container mt-5">
-    <h1>Available Hotels</h1>
-    <ul>
-        <?php if (isset($_GET['parking-included'])) {
+        <h1>Available Hotels</h1>
+        <div class="d-flex mt-5 flex-wrap">
+
+            <?php if (isset($_GET['parking-included'])) {
             foreach($hotels_with_parking as $hotel_with_parking): ?>
-            <li>
-                    <h2><?php echo $hotel_with_parking['name']; ?></h2>
-                    <p>Description: <?php echo $hotel_with_parking['description']; ?></p>
-                    <p>Parking: <?php echo $hotel_with_parking['parking'] ? 'Available' : 'Not available'; ?></p>
-                    <p>Vote: <?php echo $hotel_with_parking['vote']; ?></p>
-                    <p>Distance to center: <?php echo $hotel_with_parking['distance_to_center']; ?> km</p>
-                    </li>
-                <?php endforeach;
+            <div class="card mb-3 mr-3" style="max-width: 18rem;">
+                <div class="card-header">
+                    <?php echo $hotel_with_parking['name']; ?>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <?php echo $hotel_with_parking['description']; ?>
+                    </h5>
+                    <p class="card-text">
+                    <p>Parking:
+                        <?php echo $hotel_with_parking['parking'] ? 'Available' : 'Not available'; ?>
+                    </p>
+                    <p>Vote:
+                        <?php echo $hotel_with_parking['vote']; ?>
+                    </p>
+                    <p>Distance to center:
+                        <?php echo $hotel_with_parking['distance_to_center']; ?> km
+                    </p>
+                </div>
+            </div>
+
+            <?php endforeach;
             } else {
                  foreach($hotels as $hotel): ?>
-                    <li>
-                    <h2><?php echo $hotel['name']; ?></h2>
-                    <p>Description: <?php echo $hotel['description']; ?></p>
-                    <p>Parking: <?php echo $hotel['parking'] ? 'Available' : 'Not available'; ?></p>
-                    <p>Vote: <?php echo $hotel['vote']; ?></p>
-                    <p>Distance to center: <?php echo $hotel['distance_to_center']; ?> km</p>
-                    </li>
-                <?php endforeach;
-                
-            } ?>    
-    </ul>
+            <div class="card mb-3 mr-3" style="max-width: 18rem;">
+                <div class="card-header">
+                    <?php echo $hotel['name']; ?>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <?php echo $hotel['description']; ?>
+                    </h5>
+                    <p class="card-text">
+                    <p>Parking:
+                        <?php echo $hotel['parking'] ? 'Available' : 'Not available'; ?>
+                    </p>
+                    <p>Vote:
+                        <?php echo $hotel['vote']; ?>
+                    </p>
+                    <p>Distance to center:
+                        <?php echo $hotel['distance_to_center']; ?> km
+                    </p>
+                </div>
+            </div>
+            <?php endforeach; } ?>
+
+
+        </div>
     </div>
 </body>
+
 </html>
